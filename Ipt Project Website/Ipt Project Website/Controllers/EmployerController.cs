f0a9672cff1e9c572b88af67fd6ba1a39bdda632
anasbinwaquar <anasbinwaquar@yahoo.com>
2021-12-12 20:08:28 +0500

    Employer and User Signup and Registeration
    Added all form validations, Along with unique email and phone number checking validation before signup.

---
+++ Ipt Project Website/Ipt Project Website/Controllers/EmployerController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ipt_Project_Website.Models;
+
+namespace Ipt_Project_Website.Controllers
+{
+    public class EmployerController : Controller
+    {
+        [HttpGet]
+        public ActionResult EmployerSignUp()
+        {
+            Employer employer = new Employer();
+            return View(employer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EmployerSignUp(Employer employer)
+        {
+            DbModel dbmodel = new DbModel();
+            if (ModelState.IsValid)
+            {
+                var isEmailAlreadyExists = dbmodel.Employers.Any(x => x.Email == employer.Email);
+                if (isEmailAlreadyExists)
+                {
+                    ModelState.AddModelError("Email", "User with this email already exists");
+                    return View(employer);
+                }
+                var PhoneNumberExists = dbmodel.Employers.Any(x => x.Phone_number == employer.Phone_number);
+                if (PhoneNumberExists)
+                {
+                    ModelState.AddModelError("Phone_number", "User with this Phone number already exists");
+                    return View(employer);
+                }
+            }
+
+            using (dbmodel = new DbModel())
+            {
+                dbmodel.Employers.Add(employer);
+                dbmodel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful";
+            return View("View", employer);
+
+        }
+
+        [HttpGet]
+        public ActionResult EmployerLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EmployerLogin(FormCollection collection)
+        {
+            List<Employer> UserList = new List<Employer>();
+            using (DbModel dbmodel = new DbModel())
+            {
+                UserList = dbmodel.Employers.OrderBy(a => a.Email).ToList();
+                foreach (Employer u in UserList)
+                {
+                    if (u.Email == collection["email"] && u.Password == collection["password"])
+                    {
+                        return View("View", u);
+                    }
+                }
+            }
+            ViewBag.Message = "unSuccessful";
+            return View();
+
+        }
+    }
+}
--- Ipt Project Website/Ipt Project Website/Controllers/UserController.cs
+++ Ipt Project Website/Ipt Project Website/Controllers/UserController.cs
@@ -16,17 +16,64 @@ namespace Ipt_Project_Website.Controllers
             User user = new User();
             return View(user);
         }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]  
         public ActionResult UserSignUp(User user)
         {
-            using(DbModel dbmodel = new DbModel())
+            DbModel dbmodel = new DbModel();
+            if (ModelState.IsValid)
+            {
+                var isEmailAlreadyExists = dbmodel.Users.Any(x => x.Email == user.Email);
+                if (isEmailAlreadyExists)
+                {
+                    ModelState.AddModelError("Email", "User with this email already exists");
+                    return View(user);
+                }
+                var PhoneNumberExists = dbmodel.Users.Any(x => x.Phone_number== user.Phone_number);
+                if (PhoneNumberExists)
+                {
+                    ModelState.AddModelError("Phone_Number", "User with this Phone number already exists");
+                    return View(user);
+                }
+
+            }
+            using (dbmodel = new DbModel())
             {
                 dbmodel.Users.Add(user);
                 dbmodel.SaveChanges();
             }
             ModelState.Clear();
             ViewBag.SuccessMessage = "Registration Successful";
-            return View("UserSignUp", new User());
+            return View("View", user);
+        }
+
+
+        [HttpGet]
+        public ActionResult UserLogin()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserLogin(FormCollection collection)
+        {
+            List<User> UserList = new List<User>();
+            using (DbModel dbmodel = new DbModel())
+            {
+                UserList = dbmodel.Users.OrderBy(a => a.Email).ToList();
+                foreach(User u in UserList)
+                {
+                    if (u.Email == collection["email"] && u.Password== collection["password"])
+                    {
+                        return View("View", u);
+                    }
+                }
+            }
+            ViewBag.Message = "unSuccessful";
+            return View();
+
         }
     }
 }
