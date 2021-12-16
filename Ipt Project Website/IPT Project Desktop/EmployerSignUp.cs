aaf3c7ea88fa5b32dacaa29561443d67df08a2b4
Umaima26 <rakia.shafique@gmail.com>
2021-12-16 14:12:11 +0500

    c3

---
+++ Ipt Project Website/IPT Project Desktop/EmployerSignUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPT_Project_Desktop
+{
+    public partial class EmployerSignUp : Form
+    {
+        public EmployerSignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUpButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Ipt Project Website/IPT Project Desktop/Properties/Settings.Designer.cs
+++ Ipt Project Website/IPT Project Desktop/Properties/Settings.Designer.cs
@@ -8,21 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace IPT_Project_Desktop.Properties
-{
+namespace IPT_Project_Desktop.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\umaim\Desktop\Workspaces\IPT\Project_IPT_New\Ipt Project Website\Ipt Project Website\App_Data\Database.mdf"";Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework")]
+        public string DBConnection {
+            get {
+                return ((string)(this["DBConnection"]));
             }
         }
     }
