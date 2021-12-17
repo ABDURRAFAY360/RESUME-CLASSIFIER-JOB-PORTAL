9a3ae1f3105b2120d1b7518324dde4c0fe40aef3
Umaima26 <rakia.shafique@gmail.com>
2021-12-17 19:48:11 +0500

    c7

--- Ipt Project Website/IPT Project Desktop/EmployerSignUp.cs
+++ Ipt Project Website/IPT Project Desktop/EmployerSignUp.cs
@@ -12,6 +12,7 @@ using System.Text.RegularExpressions;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using Ipt_Project_Website.Models;
 
 namespace IPT_Project_Desktop
 {
@@ -24,24 +25,113 @@ namespace IPT_Project_Desktop
 
         private void SignUpButton_Click(object sender, EventArgs e)
         {
-            string firstName;
-            string lastName;
-            string email;
-            string password;
-            string currentEducation;
-            string employmentStatus;
-            string phoneNumber;
-            string city;
-            string country;
-            string companyName;
-            string companyContactNumber;
-            string connectionString = ConfigurationManager.ConnectionStrings["IPT_Project_Desktop.Properties.Settings.DBConnection"].ConnectionString;
-            SqlConnection con = new SqlConnection(connectionString);
-            con.Open();
-            //if(con.State == ConnectionState.Open)
-            //{
-            //    string q = @"select id from employer where ";
-            //}
+            //Regex check = new Regex(@"");
+            //Match match = check.Match();
+            Regex check;
+            Match match; 
+
+            string firstName = textBox1.Text;
+            check = new Regex(@"^[A-Za-z]+([\ A-Za-z]+)*");
+            match = check.Match(firstName);
+            if (!match.Success)
+            {
+                MessageBox.Show("Please enter a valid first name!");
+                textBox1.Clear();
+            }
+
+            string lastName = textBox2.Text;
+            check = new Regex(@"^[A-Za-z]+([\ A-Za-z]+)*");
+            match = check.Match(lastName);
+            if (!match.Success)
+            {
+                MessageBox.Show("Please enter a valid last name!");
+                textBox2.Clear();
+            }
+
+            string email = textBox3.Text;
+            
+
+            string password = textBox4.Text;
+            check = new Regex(@"^(?=.*?[a-z])(?=.*?[0-9]).{8,}$");
+            match = check.Match(password);
+            if (!match.Success)
+            {
+                MessageBox.Show("Password must be minimum eight characters in length and should contain a number!");
+                textBox4.Clear();
+            }
+
+            string currentEducation = textBox5.Text;
+
+            string employmentStatus = textBox6.Text;
+
+            string phoneNumber = textBox7.Text;
+            check = new Regex(@"^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$");
+            match = check.Match(phoneNumber);
+            if (!match.Success)
+            {
+                MessageBox.Show("Please enter a valid phone number!");
+                textBox7.Clear();
+            }
+
+            string city = textBox8.Text;
+            
+            string country = textBox9.Text;
+
+            string companyName = textBox10.Text;
+
+            string companyContactNumber = textBox11.Text;
+            check = new Regex(@"^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$");
+            match = check.Match(companyContactNumber);
+            if (!match.Success)
+            {
+                MessageBox.Show("Please enter a valid contact number!");
+                textBox11.Clear();
+            }
+
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(currentEducation) || string.IsNullOrEmpty(employmentStatus) || string.IsNullOrEmpty(phoneNumber) || string.IsNullOrEmpty(city) || string.IsNullOrEmpty(country) || string.IsNullOrEmpty(companyName) || string.IsNullOrEmpty(companyContactNumber))
+            {
+                MessageBox.Show("Please complete the form in order to Sign Up.");
+            }
+
+            DbModel dbModel = new DbModel();
+
+            var EmailAlreadyExists = dbModel.Employers.Any(x => x.Email == email);
+            if (EmailAlreadyExists)
+            {
+                MessageBox.Show("Email already exists! Please enter another email address.");
+                textBox3.Clear();
+            }
+            var PhoneNumberExists = dbModel.Employers.Any(x => x.Phone_number == phoneNumber);
+            if (PhoneNumberExists)
+            {
+                MessageBox.Show("Phone number already exists! Please enter another phone number.");
+                textBox7.Clear();
+            }
+
+            Employer employer = new Employer();
+            employer.First_name = firstName;
+            employer.Last_name = lastName;
+            employer.Email = email;
+            employer.Password = password;
+            employer.Current_education = currentEducation;
+            employer.Employment_status = employmentStatus;
+            employer.Phone_number = phoneNumber;
+            employer.City = city;
+            employer.Country = country;
+            employer.Company_name = companyName;
+            employer.Company_contact_number = companyContactNumber;
+
+            using (dbModel = new DbModel())
+            {
+                dbModel.Employers.Add(employer);
+                dbModel.SaveChanges();
+            }
+            MessageBox.Show("Sign Up successful!");
+
+            this.Hide();
+            EmployerLogin login = new EmployerLogin();
+            login.ShowDialog();
+            this.Close();
         }
 
         private void BackButton_Click(object sender, EventArgs e)
